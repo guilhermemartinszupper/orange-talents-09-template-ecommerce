ac20a9208778d1cca3d1553e43f3e93476744032
Guilherme Martins <guilherme.martins@zup.com.br>
2021-10-05 09:43:17 -0300

    configuracao inicial

---
+++ mercado_livre/src/test/java/br/com/zupedu/gui/mercado_livre/MercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupedu.gui.mercado_livre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
